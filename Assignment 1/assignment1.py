7933126554aa9cd4c975d02adb0ebe3bdc48c560
Andrés Reverón Molina <andressser@gmail.com>
2020-02-28 18:47:35 +0100

    switch to B&W

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -7,15 +7,29 @@ video = cv2.VideoCapture('video1.mp4')
 if not video.isOpened():
     print("Error opening video stream or file")
 
+# Get video duration
+fps = video.get(cv2.CAP_PROP_FPS)
+frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+duration_seconds = frame_count/fps
+
 # Read until video is completed
 while video.isOpened():
     # Capture frame-by-frame
     ret, frame = video.read()
 
     if ret:
-        # Display the resulting frame
-        resizedFrame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-        cv2.imshow("frame", resizedFrame)
+        # Get timestamp of current frame (in seconds)
+        frame_timestamp = video.get(cv2.CAP_PROP_POS_MSEC) / 1000
+        # Resize the frame
+        frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        # Switch between color and B&W
+        if 1 < frame_timestamp < 2 or 3 < frame_timestamp < 4:
+            cv2.putText(frame, 'B&W', (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, cv2.LINE_AA)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        elif frame_timestamp < 4:
+            cv2.putText(frame, 'Color', (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, cv2.LINE_AA)
+
+        cv2.imshow("frame", frame)
 
         # Press Q on keyboard to  exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
