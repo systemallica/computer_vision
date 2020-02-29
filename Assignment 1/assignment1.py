03f2524e94511f3685ab9704b8f4c7046b5dbee7
Andrés Reverón Molina <andressser@gmail.com>
2020-02-29 12:33:58 +0100

    feat: smoothing filters

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -7,11 +7,6 @@ video = cv2.VideoCapture('video1.mp4')
 if not video.isOpened():
     print("Error opening video stream or file")
 
-# Get video duration
-fps = video.get(cv2.CAP_PROP_FPS)
-frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-duration_seconds = frame_count/fps
-
 # Read until video is completed
 while video.isOpened():
     # Capture frame-by-frame
@@ -22,12 +17,30 @@ while video.isOpened():
         frame_timestamp = video.get(cv2.CAP_PROP_POS_MSEC) / 1000
         # Resize the frame
         frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-        # Switch between color and B&W
+
+        # Apply effect based on current timestamp
         if 1 < frame_timestamp < 2 or 3 < frame_timestamp < 4:
+            # Black and white
             cv2.putText(frame, 'B&W', (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, cv2.LINE_AA)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         elif frame_timestamp < 4:
+            # Color
             cv2.putText(frame, 'Color', (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, cv2.LINE_AA)
+        elif 6 > frame_timestamp > 4:
+            # Gaussian smoothing
+            kernel_size = 5
+            frame = cv2.GaussianBlur(frame, (kernel_size, kernel_size), 0)
+            cv2.putText(frame, 'Gaussian Blur. Kernel size: ' + str(kernel_size), (10, 450),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        elif 8 > frame_timestamp > 6:
+            # Gaussian smoothing
+            kernel_size = 11
+            frame = cv2.GaussianBlur(frame, (kernel_size, kernel_size), 0)
+            cv2.putText(frame, 'Gaussian Blur. Kernel size: ' + str(kernel_size), (10, 450),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        elif frame_timestamp > 8:
+            frame = cv2.bilateralFilter(frame, 15, 100, 100)
+            cv2.putText(frame, 'Bilateral filter(edges are preserved)', (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
         cv2.imshow("frame", frame)
 
